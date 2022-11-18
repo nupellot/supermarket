1a6f67b17a0b5b002778769778798356168539bf
Dmitriy Korotkov <nupellot@gmail.com>
2022-11-18 04:28:57 +0300

    Начал переработку БД-архитектуры

--- database/connection.py
+++ database/connection.py
@@ -6,11 +6,26 @@ from pymysql.err import OperationalError
 
 
 class UseDatabase:
+    """Класс для подключения к БД и выполнения sql-запросов."""
 
     def __init__(self, config: dict):
+        """
+        Инициализация объекта подключения.
+        Args:
+             config: dict - Конфиг дял подключения к БД.
+        """
+
         self.config = config
 
+        # self.conn: Optional[Connection] = None
+        # self.cursor: Optional[Cursor] = None
     def __enter__(self) -> Optional[Cursor]:
+        """
+        Реализует логику входа в контекстный менеджер.
+        Создает соединение к БД и возвращает курсор для выполнения запросов.
+        Return:
+            Курсор для работы с БД или NULL.
+        """
         try:
             self.conn = connect(**self.config)
             self.cursor = self.conn.cursor()
@@ -25,6 +40,15 @@ class UseDatabase:
             return None
 
     def __exit__(self, exc_type, exc_val, exc_tr) -> bool:
+        """
+        Реализует логику выхода из контекстого менеджера для работы с БД.
+        Закрывает соединение и курсор.
+        Возвращаемое значение всего True для обеспечения сокрытия списка ошибок в консоли.
+        Args:
+            exc_type: Тип возможной ошибки при работе менеджера.
+            exc_val: Значение возможной ошибки при работе менеджера.
+            exc_tr: Traceback (подробный текст ошибки) при работе менеджера.
+        """
         if exc_val:
             # print(exc_val)
             # print(exc_type)
