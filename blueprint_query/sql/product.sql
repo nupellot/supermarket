fc0ca8f4dcac48407e2095842f33e24b8abd3450
Dmitriy Korotkov <nupellot@gmail.com>
2022-11-21 21:32:38 +0300

    Изменил поисковую систему в соответсвии с современными рекомендациями руководителя продукта Ивана Малова

--- blueprint_query/sql/product.sql
+++ blueprint_query/sql/product.sql
@@ -1,3 +1,3 @@
 select prod_id AS "ID", prod_name AS "Name", prod_measure AS "Measure", prod_price AS "Price"
 from product
-where prod_name='$input_product'
+where prod_name LIKE '%$input_product%'
