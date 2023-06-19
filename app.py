443d741732f6c4fd985218b66bd7cf2f7183097f
Dmitriy Korotkov <nupellot@gmail.com>
2023-06-19 13:01:24 +0300

    Временно избавился от Redis

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, render_template, session, request, redirect, url_for
 from auth.routes import blueprint_auth
 from catalog.routes import blueprint_catalog
-from market.routes import blueprint_market
+# from market.routes import blueprint_market
 from orderlist.routes import blueprint_orderlist
 from report.routes import blueprint_report
 from basket.routes import blueprint_basket
@@ -15,7 +15,7 @@ app.secret_key = 'SuperKey'
 
 app.register_blueprint(blueprint_auth, url_prefix='/auth')
 app.register_blueprint(blueprint_report, url_prefix='/report')
-app.register_blueprint(blueprint_market, url_prefix='/market')
+# app.register_blueprint(blueprint_market, url_prefix='/market')
 app.register_blueprint(blueprint_basket, url_prefix='/basket')
 app.register_blueprint(blueprint_catalog, url_prefix='/catalog')
 app.register_blueprint(blueprint_orderlist, url_prefix='/orderlist')
