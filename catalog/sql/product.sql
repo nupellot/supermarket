58a905f44c0b70078c98902025a3663c38844e7d
Dmitriy Korotkov <nupellot@gmail.com>
2023-06-21 01:58:10 +0300

    Исправил поисковый запрос на более адекватный

--- catalog/sql/product.sql
+++ catalog/sql/product.sql
@@ -1,4 +1,4 @@
 select prod_id, prod_name, prod_measure, prod_price, prod_img, prod_description
 from product
-where prod_name LIKE '%$input_product%'
+where prod_name LIKE '%$query%'
 ORDER BY prod_name
